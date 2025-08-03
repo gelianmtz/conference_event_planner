c451732c8b06f5b34751106ab180e0399d7c43b7
gelianmtz <ger.elian.martinez@outlook.com>
2025-08-02 23:29:14 -0700

    practice project

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -1,48 +1,50 @@
 // venueSlice.js
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const venueSlice = createSlice({
-  name: "venue",
+  name: 'venue',
   initialState: [
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
-      name: "Conference Room (Capacity:15)",
+      img: 'https://pixabay.com/images/download/chairs-2181916_640.jpg',
+      name: 'Conference Room (Capacity:15)',
       cost: 3500,
-      quantity: 0,
+      quantity: 0
     },
     {
-      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
-      name: "Auditorium Hall (Capacity:200)",
+      img: 'https://pixabay.com/images/download/event-venue-1597531_640.jpg',
+      name: 'Auditorium Hall (Capacity:200)',
       cost: 5500,
-      quantity: 0,
+      quantity: 0
     },
     {
-      img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
-      name: "Presentation Room (Capacity:50)",
+      img: 'https://pixabay.com/images/download/convention-center-3908238_640.jpg',
+      name: 'Presentation Room (Capacity:50)',
       cost: 700,
-      quantity: 0,
+      quantity: 0
     },
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
-      name: "Large Meeting Room (Capacity:10)",
+      img: 'https://pixabay.com/images/download/chairs-2181916_640.jpg',
+      name: 'Large Meeting Room (Capacity:10)',
       cost: 900,
-      quantity: 0,
+      quantity: 0
     },
     {
-      img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
-      name: "Small Meeting Room (Capacity:5)",
+      img: 'https://pixabay.com/images/download/laptops-593296_640.jpg',
+      name: 'Small Meeting Room (Capacity:5)',
       cost: 1100,
-      quantity: 0,
-    },
-  
+      quantity: 0
+    }
   ],
   reducers: {
-   
     incrementQuantity: (state, action) => {
       const { payload: index } = action;
       if (state[index]) {
-        if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
-          return;        }
+        if (
+          state[index].name === ' Auditorium Hall (Capacity:200)' &&
+          state[index].quantity >= 3
+        ) {
+          return;
+        }
         state[index].quantity++;
       }
     },
@@ -51,8 +53,8 @@ export const venueSlice = createSlice({
       if (state[index] && state[index].quantity > 0) {
         state[index].quantity--;
       }
-    },
-  },
+    }
+  }
 });
 
 export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
